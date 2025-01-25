7a9a89b278a98c7250075cdc2bc0d3978ec6b446
Hermule <l.hermeler@freenet.de>
2025-01-25 23:55:07 +0100

    Add assignment 7, temp conversions

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temperatureInFahrenheit) {
+  let temperatureinCelsius = (temperatureInFahrenheit - 32) * (5/9);
+  return Math.round(temperatureinCelsius * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temperatureinCelsius) {
+  let temperatureInFahrenheit = (temperatureinCelsius * 9/5) +32;
+  return Math.round(temperatureInFahrenheit * 10) / 10;
 };
 
 // Do not edit below this line
