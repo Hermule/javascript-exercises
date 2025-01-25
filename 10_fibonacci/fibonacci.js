0b518cc549accb26f2b69381190d76c7428361fe
Hermule <l.hermeler@freenet.de>
2025-01-26 00:17:06 +0100

    Complete the fibonacci sequence

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,24 @@
-const fibonacci = function() {
+const fibonacci = function(indexOfFibonacciSequence) {
+    let lastValue = 0;
+    let currentValue = 0;
+    let helperInt = 0;
+    let parsedIndex = parseInt(indexOfFibonacciSequence); 
 
+    if (parsedIndex == NaN || parsedIndex < 0){
+        return 'OOPS';
+    }
+    if (parsedIndex == 1){
+        return 1;
+    }
+    for (let i = 0; i < parsedIndex; i++){
+        if (i == 1){
+            currentValue = 1;
+        }
+        helperInt = currentValue;
+        currentValue += lastValue;
+        lastValue = helperInt;
+    }
+    return currentValue;
 };
 
 // Do not edit below this line
